f575eb225604021749973e3fd8be072b721ceae9
louis frederic schefer <lf.schefer@gmail.com>
2022-11-27 23:30:23 +0100

    back back back

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,6 @@
 class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+    @itinerary = Itinerary.new
+  end
 end
--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,2 +1,27 @@
 class ItinerariesController < ApplicationController
+
+  def new
+    @itinerary = Itinerary.new
+  end
+
+  def create
+
+  end
+
+  def index
+    @itineraries = Itinerary.all
+  end
+
+  def show
+    @itinerary = Itinerary.find(params[:id])
+    @image = Image.where(itinerary_id: params[:id])
+  end
+
+
+  private
+
+  def itinerary_params
+    params.require(:itinerary).permit(:start_address, :end_address, :start_latitude, :start_longitude, :end_latitude, :end_longitude)
+  end
+
 end
