33df2d1565e4ac9a78bf68f194d996941483b88b
louis frederic schefer <lf.schefer@gmail.com>
2022-11-28 02:18:49 +0100

    first api gg marche

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,7 @@
 class Itinerary < ApplicationRecord
 require 'uri'
 require 'net/http'
+require 'json'
 
 
   belongs_to :user
@@ -10,9 +11,11 @@ require 'net/http'
 
   def calculate_itinerary
 
-    uri = URI('https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=ENV[GOOGLE_API_KEY]')
+    uri = URI("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=#{ENV['GOOGLE_API_KEY']}")
     res = Net::HTTP.get_response(uri)
     puts res.body if res.is_a?(Net::HTTPSuccess)
+    data = JSON.parse(res.body)
+    #  extraire l information pertinente et la persister sur l itineraire
     raise
   end
 end
