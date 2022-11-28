8841fbe8f9e3b7997617916f0e47a84da909420d
louis frederic schefer <lf.schefer@gmail.com>
2022-11-29 00:03:01 +0100

    fffixxxxxxxxxxxx

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -12,7 +12,7 @@ class ItinerariesController < ApplicationController
     if @itinerary.save
       redirect_to itinerary_path(@itinerary)
     else
-      redirect_to images_path notice: "You must add start and end point"
+      redirect_to images_path notice: "You must add start and end point end mode"
     end
   end
 
