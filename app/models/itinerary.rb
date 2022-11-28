b5435afc34c03e0d04ba819161b2bf35f6aac40c
louis frederic schefer <lf.schefer@gmail.com>
2022-11-28 02:02:41 +0100

    api

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,3 +1,18 @@
 class Itinerary < ApplicationRecord
+require 'uri'
+require 'net/http'
+
+
   belongs_to :user
+  after_save :calculate_itinerary
+
+  private
+
+  def calculate_itinerary
+
+    uri = URI('https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=ENV[GOOGLE_API_KEY]')
+    res = Net::HTTP.get_response(uri)
+    puts res.body if res.is_a?(Net::HTTPSuccess)
+    raise
+  end
 end
