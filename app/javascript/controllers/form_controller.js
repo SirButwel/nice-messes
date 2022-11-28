d3f3f15396383d83f0b2d0f645b0d97655f7cf08
louis frederic schefer <lf.schefer@gmail.com>
2022-11-28 23:25:51 +0100

    v2

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,6 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["start", "end", "mode", "submit"]
   connect() {
+    console.log("connected")
   }
 
   end() {
