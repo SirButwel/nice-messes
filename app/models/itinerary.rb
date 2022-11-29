595fd1b49707310ab724f4abdf7c12b27a55e193
Marie Santi <marie.santi@penninghen.fr>
2022-11-29 17:45:30 +0100

    Itinerary meteo

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,3 +1,28 @@
+require 'open-uri'
+require 'json'
+require 'time'
+
 class Itinerary < ApplicationRecord
   belongs_to :user
+  before_save :weather_api
+  geocoded_by :end_addressend
+
+  private
+  def weather_api
+
+    url = "https://api.meteo-concept.com/api/forecast/daily/0?token=#{ENV['WEATHER_API_KEY']}&insee=#{destination_postcode}"
+
+        URI.open("https://api.meteo-concept.com/api/forecast/daily/0?token=#{ENV['WEATHER_API_KEY']}&insee=35238") do |stream|
+        city, forecast = JSON.parse(stream.read).values_at('city','forecast')
+          p forecast
+          update_weather_data(forecast)
+      end
+
+
+  end
+  def update_weather_data(data)
+    self.weather_data = data
+
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 require 'faker'
+
+require 'open-uri'
+require 'json'
+require 'time'
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
@@ -14,6 +18,8 @@ andris = User.new(
     password: "Password"
   )
 andris.save!
+
+puts 'Create test itinerary'
 
 Itinerary.new(start_address: "16 Villa Gaudelet, 75011 Paris", end_address: "30 rue ballu 75009 paris",
               start_latitude: 48.8648601,
