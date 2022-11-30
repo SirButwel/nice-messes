a3b8388f31d3fbf8224056a2adfbc7277be492d0
Andris Asper <andris.asper@gmail.com>
2022-11-30 12:07:49 +0100

    1st commit

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -13,17 +13,16 @@ class Itinerary < ApplicationRecord
 
   private
   def weather_api
-
     url = "https://api.meteo-concept.com/api/forecast/daily/0?token=#{ENV['WEATHER_API_KEY']}&insee=#{destination_postcode}"
-
-        URI.open("https://api.meteo-concept.com/api/forecast/daily/0?token=#{ENV['WEATHER_API_KEY']}&insee=35238") do |stream|
-        city, forecast = JSON.parse(stream.read).values_at('city','forecast')
-          p forecast
-          update_weather_data(forecast)
+      URI.open("https://api.meteo-concept.com/api/forecast/daily/0?token=#{ENV['WEATHER_API_KEY']}&insee=35238") do |stream|
+        city, forecast = JSON.parse(stream.read).values_at('city', 'forecast')
+        p forecast
+        update_weather_data(forecast)
       end
 
 
   end
+
   def update_weather_data(data)
     self.weather_data = data
 
